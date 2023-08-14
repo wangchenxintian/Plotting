b80b103e6e86953e8c81f029602d965b4b5ab158
wangchenxintian <141798467+wangchenxintian@users.noreply.github.com>
2023-08-14 19:54:39 +0800

    Update plot.R

--- plot.R
+++ plot.R
@@ -66,3 +66,117 @@ upset(regions ,sets = c("South Africa","Macau","Singapore","Brazil", "Hong Kong"
 #The intersection results are grouped, nintersects the number of crossing points and cutoff the threshold of crossing results
 upset(movies,nintersects = 70, group.by = "sets", cutoff = 7)
 
+
+#bubble matrix
+data<-read.csv("bubble.csv", header = T)        
+library(ggplot2)
+library(tidyverse)
+data_melt<- gather(data, key=ARGtype, value=Value, Vancomycin:Aminoglycoside)
+names(data_melt) = c("color", 'Ecotype', 'ARGtype', 'Value')
+data_melt$Ecotype <- as.character(data_melt$Ecotype)
+data_melt$Ecotype <- factor(data_melt$Ecotype, levels=unique(data_melt$Ecotype))
+data_melt$ARGtype <- as.character(data_melt$ARGtype)
+data_melt$ARGtype <- factor(data_melt$ARGtype, levels=unique(data_melt$ARGtype))
+cbPalette <- c( "#FC690C","#FEFF1A","#1AFEFF","#00A9E6","#006400","#8CB2D2" )
+p<-ggplot(data_melt, aes(x = Ecotype, y = ARGtype, size = Value, color=color)) + geom_point() +   scale_colour_manual(values=cbPalette) +
+  theme_bw(base_size = 12)+
+  theme( panel.background = element_blank(),
+         panel.grid.major = element_line(colour=NA),
+         panel.border = element_rect(colour="black",fill=NA))
+p
+p + xlab("Ecotype(s)") + theme(axis.title.x = element_text(size = 20, family = "myFont", color = "black", face = "bold", vjust = 0.5, hjust = 0.5))
+p + theme(axis.text.x=element_text(size=13,angle = 90,color = "black"),axis.text.y=element_text(size=13,color = "black",),panel.grid.major=element_line(colour=NA))  +
+  guides(color = FALSE)  + labs(size="Abundance") + xlab("Ecotype(s)") + ylab("ARG Type") +
+  scale_size(range = c(3, 12))
+
+p <- p +  scale_y_continuous(trans = "reverse", breaks = unique(data_melt$ARGtype))
+
+
+#boxplot
+library(ggplot2)
+  install.packages("tydir")
+  library(tidyr)
+  
+B.data<-read.csv("D:/R/boxplot.csv",
+                   header=TRUE,check.names = FALSE)
+  str(B.data)
+  
+  B.data$SampleID<-NULL
+  B.data_long <- gather(B.data, group, value, `Animal Feces`:Unknown, factor_key=TRUE)
+  
+  ggplot(B.data_long, aes(x=group,y=value,color=group)) +
+    geom_boxplot(outlier.shape = NA) + 
+    labs(fill = "group") + 
+    xlab("Group")+ylab("Percentage(100%)")+
+    geom_point(aes(x=group,y=value,color=group),position=position_jitterdodge(),alpha=0.7) +
+    theme_bw(base_size = 12)+
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank())
+  
+  #ggplot(B.data_long, aes(x=group,y=value,fill=factor(group))) +
+    geom_boxplot() + 
+    labs(fill = "group") + 
+    xlab("Group")+ylab("Percentage(100%)")+
+    geom_point(position=position_jitterdodge(),alpha=0.7) +
+    theme_bw(base_size = 12)+
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank())
+  
+  ####################### New ######################
+  AF<-B.data$`Animal Feces`
+  OutVals <- boxplot(AF)$out
+  which(AF %in% OutVals)
+  AF <- AF[-which(AF %in% OutVals)]
+  mean(AF)
+  
+  HF<-B.data$`Human Feces`
+  OutVals <- boxplot(HF)$out
+  which(HF %in% OutVals)
+  HF <- HF[-which(HF %in% OutVals)]
+  mean(HF)
+  
+  NATURAL<-B.data$Natural
+  mean(NATURAL)
+  
+  WWTP<-B.data$WWTP
+  mean(WWTP)
+  
+  UNKNOWN<-B.data$Unknown
+  OutVals <- boxplot(UNKNOWN)$out
+  which(UNKNOWN %in% OutVals)
+  UNKNOWN <- UNKNOWN[-which(UNKNOWN %in% OutVals)]
+  mean(UNKNOWN)
+  
+  value<-c(AF,HF,NATURAL,WWTP,UNKNOWN)
+  group<-c(rep("Animal Feces",length(AF)),
+           rep("Human Feces",length(HF)),
+           rep("Natural",length(NATURAL)),
+           rep("WWTP",length(WWTP)),
+           rep("Unknown",length(UNKNOWN)))
+  B.data.new<-as.data.frame(cbind(group,value))
+  B.data.new$value<-as.character(B.data.new$value)
+  B.data.new$value<-as.numeric(B.data.new$value)
+  
+  
+  ggplot(B.data.new, aes(x=group,y=value,color=group)) +
+    geom_boxplot(outlier.shape = NA) + 
+    labs(fill = "group") + 
+    xlab("Group")+ylab("Percentage(100%)")+
+    geom_point(aes(x=group,y=value,color=group),position=position_jitterdodge(),alpha=0.5) +
+    theme_bw(base_size = 12)+
+    annotate(label = sprintf("0"),
+             geom = "text", x = "Animal Feces", y = 5, size = 4)+
+    annotate(label = sprintf("0"),
+             geom = "text", x = "Human Feces", y = 5, size = 4)+
+    annotate(label = sprintf("57.42"),
+             geom = "text", x = "Natural", y = 62, size = 4)+
+    annotate(label = sprintf("2.90"),
+             geom = "text", x = "Unknown", y = 15, size = 4)+
+    annotate(label = sprintf("36.03"),
+             geom = "text", x = "WWTP", y = 40, size = 4)+
+    coord_cartesian(ylim=c(0, 100))
+                           
+                    
+                         
